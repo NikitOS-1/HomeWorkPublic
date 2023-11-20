4f615ac0552c96c19bec0fb385430486be9ed65a
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-20 16:24:26 +0200

    Add HW 6 (compleated)

--- main.js
+++ main.js
@@ -1,256 +1,138 @@
-// task - 1
-// Створити масив, довжину та елементи якого задає користувач.
+// Ця дз складається з чотирьох невеликих завдань, за реалізацію кожної з них можна отримати 25 балів:
 
-// Відсортувати масив за зростанням.
+// 1.Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
 
-// Видалити елементи з масиву з 2 по 4 (включно!).
+// 2.Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
 
-// У міру змін виводити вміст масиву на сторінку.
+// 3.Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
 
-function getLengthArr() {
-  let arrayLength = Number(prompt("Enter an array of the length you want"));
-  if (isNaN(arrayLength)) {
-    alert("You only need to write the Number, Please!");
-    return getLengthArr();
-  } else if (
-    arrayLength === 0 ||
-    arrayLength === false ||
-    arrayLength === null
-  ) {
-    alert("Okey, Bye bye!");
-    return null;
-  }
-  return arrayLength;
-}
+// 4.Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
 
-function addDataToArray(length) {
-  let arr = [];
-  for (let i = 0; i <= length - 1; i++) {
-    let getItem = prompt("Enter the element you want to add to the array");
-    arr.push(getItem);
-  }
-  return arr;
-}
+// _______________________________________________________________
+// 1.Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
 
-function arrChanges(arr) {
-  let choise = Number(
-    prompt(
-      "How do you want to modify this array? Choose your option Please\n enter - 1) Sort\n enter - 2) Remove elements from array 2 to 4\n ----and then I show this date on this page"
-    )
-  );
-  if (isNaN(choise)) {
-    alert("You only need to write the Number, Please!");
-    return arrChanges(arr);
-  } else if (choise === 0 || choise === false || choise === null) {
-    alert("Okey, Bye bye!");
-    return null;
-  }
-
-  if (choise === 1) {
-    return arr.sort((a, b) => a - b);
-  }
-  if (choise === 2) {
-    if (arr.length <= 4) {
-      alert(
-        "The length of your array is less than 4 \n Choise another option!"
-      );
-      return arrChanges(arr);
-    }
-    let removedElemOnArr = arr.splice(2, 4);
-    return removedElemOnArr;
-  }
-}
-
-function app() {
-  alert(
-    "I can help you create an array and make some changes to it and then display your data on the page!"
-  );
-  const arrLength = getLengthArr();
-  if (arrLength === null) return;
-
-  const arrData = addDataToArray(arrLength);
-  if (arrData === null) return;
-
-  alert(`Length your Array = ${arrLength} \n Your Data = [${arrData.join()}]`);
-  let beforeArr = arrData.join();
-
-  const arrChange = arrChanges(arrData);
-  if (arrChange === null) {
-    return;
-  } else {
-    document.write(`Your Data before change = [${beforeArr}]`);
-    document.write(`---------`);
-    document.write(`Your Data after changed = [${arrChange.join()}]`);
-  }
-}
-
-app();
-
-// task - 2
-
-// Дан масив [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
-// 1.Знайти суму та кількість позитивних елементів.
-// 2.Знайти мінімальний елемент масиву та його порядковий номер.
-// 3.Знайти максимальний елемент масиву та його порядковий номер.
-// 4.Визначити кількість негативних елементів.
-// 5.Знайти кількість непарних позитивних елементів.
-// 6.Знайти кількість парних позитивних елементів.
-// 7.Знайти суму парних позитивних елементів.
-// 8.Знайти суму непарних позитивних елементів.
-// 9.Знайти добуток позитивних елементів.
-// 10.Знайти найбільший серед елементів масиву, остальні обнулити.
-
-let data = [
-  16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54,
-  76, -4, 12, -35, 4, 47,
+const array = [
+  1,
+  2,
+  3,
+  ["hello", "world", ["df", 89, 3, ["32", 45]]],
+  "123",
+  4,
+  5,
+  2,
+  10,
 ];
 
-// 1.Знайти суму та кількість позитивних елементів.
-function findSumPositivElem(arr) {
-  let array = [];
+/**
+ *
+ * @param {Array} arr - Receives an array of data
+ * @returns {Number} - Returns the arithmetic average of all typeof Number elements in an array
+ */
+function middleValueNumber(arr) {
+  let numbersOfArray = arr
+    .flat(Infinity)
+    .filter((el) => typeof el === "number");
 
-  for (const num of arr) {
-    if (num > 0) array.push(num);
+  if (numbersOfArray.length === 0) {
+    return "Your array dont have numbers";
   }
-  let length = array.length;
-  let sum = array.reduce((acc, num) => acc + num);
 
-  return console.log(
-    `Кількість позитивних елементів: ${length}.  Загальна сума = ${sum}.`
-  );
+  let sum = numbersOfArray.reduce((acc, num) => num + acc, 0);
+  let middleValue = sum / numbersOfArray.length;
+
+  return middleValue;
 }
 
-// 2.Знайти мінімальний елемент масиву та його порядковий номер.
-function findMinNumInd(arr) {
-  let minNum = arr[0];
-  let indexMinNum = 0;
+// _______________________________________________________________
+// 2.Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, вказаної в змінній znak.Обидва числа і знак виходять від користувача.
 
-  for (const num of arr) {
-    if (num < minNum) {
-      minNum = num;
-    }
+/**
+ *
+ * @param {Number} x - Your first number
+ * @param {String} znak - Use an arithmetic operator
+ * @param {Number} y - Your second number
+ * @returns {Number} - Returns the sum of x and y with the selected arithmetic operator
+ */
+function doMath(x, znak, y) {
+  let result;
+  if (x === undefined || znak === undefined || y === undefined) {
+    return "Error";
   }
-
-  arr.map((number, index) => {
-    if (number === minNum) {
-      return (indexMinNum = index);
-    }
-  });
-  console.log(
-    `Mінімальний елемент масиву це ${minNum} , його порядковий індекс ${indexMinNum}`
-  );
-}
-
-// 3.Знайти максимальний елемент масиву та його порядковий номер.
-function findMaxNumInd(arr) {
-  let minNum = arr[0];
-  let indexMinNum = 0;
-
-  for (const num of arr) {
-    if (num > minNum) {
-      minNum = num;
-    }
-  }
-
-  arr.map((number, index) => {
-    if (number === minNum) {
-      return (indexMinNum = index);
-    }
-  });
-  console.log(
-    `Максимальний елемент масиву це ${minNum} , його порядковий індекс ${indexMinNum}`
-  );
-}
-
-// 4.Визначити кількість негативних елементів.
-function findNegotiveElem(arr) {
-  let array = [];
-
-  for (const num of arr) {
-    if (num < 0) array.push(num);
-  }
-  let length = array.length;
-
-  return console.log(`Кількість негативних елементів: ${length}. `);
-}
-
-// 5.Знайти кількість непарних позитивних елементів.
-function findPosNotPair(arr) {
-  let array = [];
-
-  for (const num of arr) {
-    if (num > 0 && num % 2 === 1) array.push(num);
-  }
-  let length = array.length;
-
-  return console.log(`Кількість непарних позитивних елементів: ${length}. `);
-}
-
-// 6.Знайти кількість парних позитивних елементів.
-function findPosPair(arr) {
-  let array = [];
-
-  for (const num of arr) {
-    if (num > 0 && num % 2 === 0) array.push(num);
-  }
-  let length = array.length;
-
-  return console.log(`Кількість непарних позитивних елементів: ${length}.`);
-}
-
-// 7.Знайти суму парних позитивних елементів.
-function findPosSumPair(arr) {
-  let array = [];
-
-  for (const num of arr) {
-    if (num > 0 && num % 2 === 0) array.push(num);
-  }
-
-  let sum = array.reduce((acc, num) => acc + num);
-
-  return console.log(`Cума парних позитивних елементів: ${sum}.`);
-}
-
-// 8.Знайти суму непарних позитивних елементів.
-function findPosSumNotPair(arr) {
-  let array = [];
-
-  for (const num of arr) {
-    if (num > 0 && num % 2 === 1) array.push(num);
-  }
-
-  let sum = array.reduce((acc, num) => acc + num);
-
-  return console.log(`Cума непарних позитивних елементів: ${sum}.`);
-}
-
-// 9.Знайти добуток позитивних елементів.
-function findPosMult(arr) {
-  let array = [];
-
-  for (const num of arr) {
-    if (num > 0) array.push(num);
-  }
-
-  let sum = array.reduce((acc, num) => acc * num);
-
-  return console.log(`Добуток позитивних елементів = ${sum}.`);
-}
-
-// 10.Знайти найбільший серед елементів масиву, остальні обнулити.
-function findMaxNumRemoveOther(arr) {
-  let maxNum = arr[0];
-  let newArray = [];
-  for (const num of arr) {
-    if (num > maxNum) maxNum = num;
-  }
-  for (const num of arr) {
-    if (num === maxNum) {
-      newArray.push(num);
+  if (znak == "+") {
+    result = x + y;
+  } else if (znak == "-") {
+    result = x - y;
+  } else if (znak == "*") {
+    result = x * y;
+  } else if (znak == "/") {
+    if (y === 0) {
+      return "You can't divide by 0";
     } else {
-      newArray.push(0);
+      result = x / y;
     }
+  } else if (znak == "%") {
+    result = (x * y) / 100;
+  } else if (znak == "^") {
+    result = Math.pow(x, y);
+  } else {
+    return "You can't use this operator";
+  }
+  return result;
+}
+
+// _______________________________________________________________
+// 3.Написати функцію заповнення даними користувача двомірного масиву. Довжину основного масиву і внутрішніх масивів задає користувач. Значення всіх елементів всіх масивів задає користувач.
+
+/**
+ *
+ * @returns {Array} returns a two-dimensional array filled with client data
+ */
+function fillArrayOfArray() {
+  let lengthArrayGeneral = Number(
+    prompt("Enter the length of the general array")
+  );
+  let lengthArraySecond = Number(
+    prompt("Enter the length of the internal array")
+  );
+
+  let arrayOfArrays = [];
+
+  if (isNaN(lengthArrayGeneral) || isNaN(lengthArraySecond))
+    return "You need use only Number";
+
+  for (let i = 0; i < lengthArrayGeneral; i++) {
+    let secondArray = [];
+
+    for (let j = 0; j < lengthArraySecond; j++) {
+      let item = prompt(
+        `Enter a value for the (Array:${i + 1}, Element:${j + 1})`
+      );
+      secondArray.push(item);
+    }
+
+    arrayOfArrays.push(secondArray);
   }
 
-  return console.log(newArray.join());
+  return console.log(arrayOfArrays);
+}
+
+// _______________________________________________________________
+// 4.Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
+/**
+ *
+ * @param {String} string - Enter the line from which you want to remove the letter
+ * @param {Array} arrayLetters - Enter an array with the letters you want to delete
+ * @returns {String} - Returns a string with letters removed
+ */
+function removeLetterFromString(string, arrayLetters) {
+  if (typeof string !== "string" || !Array.isArray(arrayLetters)) {
+    return "Please enter the words and then the letters you want to delete";
+  }
+
+  const changedString = string
+    .split("")
+    .filter((letter) => !arrayLetters.includes(letter))
+    .join("");
+
+  return changedString;
 }
