f19bdf34459bb50ca38364480a3787ca39986550
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-12-24 23:42:29 +0200

    compleated HW 13

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
-let currentIndex = 0;
-let totalImages = document.querySelectorAll(".slider-image").length;
+function submitForm() {
+  var formData = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    dob: document.getElementById("dob").value,
+    gender: document.querySelector('input[name="gender"]:checked').value,
+    city: document.getElementById("city").value,
+    address: document.getElementById("address").value,
+    languages: Array.from(
+      document.querySelectorAll('input[name="languages"]:checked')
+    ).map((checkbox) => checkbox.value),
+  };
 
-function showImage(index) {
-  let translateValue = -index * 100 + "%";
-  currentIndex = index;
-
-  document.getElementById("image-slider").style.transform =
-    "translateX(" + translateValue + ")";
-  document.getElementById("prev").style.display =
-    currentIndex === 0 ? "none" : "block";
-  document.getElementById("next").style.display =
-    currentIndex === totalImages - 1 ? "none" : "block";
-}
-
-function nextImage() {
-  if (currentIndex < totalImages - 1) {
-    showImage(currentIndex + 1);
+  var table = document.getElementById("dataTable");
+  table.innerHTML = "<tr><th>Поле</th><th>Значення</th></tr>";
+  for (var key in formData) {
+    table.innerHTML +=
+      "<tr><td>" + key + "</td><td>" + formData[key] + "</td></tr>";
   }
-}
 
-function prevImage() {
-  if (currentIndex > 0) {
-    showImage(currentIndex - 1);
-  }
+  document.getElementById("registrationForm").style.display = "none";
+  table.style.display = "table";
 }
-
-showImage(0);
