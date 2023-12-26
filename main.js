080f486a43a371c79dee03233585507cfbc08390
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-12-26 16:42:23 +0200

    compleated HW 16

--- main.js
+++ main.js
@@ -1,23 +1,128 @@
-function submitForm() {
-  var formData = {
-    firstName: document.getElementById("firstName").value,
-    lastName: document.getElementById("lastName").value,
-    dob: document.getElementById("dob").value,
-    gender: document.querySelector('input[name="gender"]:checked').value,
-    city: document.getElementById("city").value,
-    address: document.getElementById("address").value,
-    languages: Array.from(
-      document.querySelectorAll('input[name="languages"]:checked')
-    ).map((checkbox) => checkbox.value),
-  };
+document.addEventListener("DOMContentLoaded", function () {
+  displayTasks();
+});
 
-  var table = document.getElementById("dataTable");
-  table.innerHTML = "<tr><th>Поле</th><th>Значення</th></tr>";
-  for (var key in formData) {
-    table.innerHTML +=
-      "<tr><td>" + key + "</td><td>" + formData[key] + "</td></tr>";
+function addTask() {
+  let taskInput = document.getElementById("task");
+  let task = taskInput.value.trim();
+
+  if (task === "") {
+    alert("Please enter a task.");
+    return;
   }
 
-  document.getElementById("registrationForm").style.display = "none";
-  table.style.display = "table";
+  let tasks = getTasksFromLocalStorage();
+
+  tasks.push({ text: task, completed: false });
+
+  saveTasksToLocalStorage(tasks);
+
+  taskInput.value = "";
+
+  displayTasks();
+}
+
+function editTask(index) {
+  let tasks = getTasksFromLocalStorage();
+  let updatedTask = prompt("Edit task:", tasks[index].text);
+
+  if (updatedTask !== null) {
+    tasks[index].text = updatedTask;
+
+    saveTasksToLocalStorage(tasks);
+
+    displayTasks();
+  }
+}
+
+function deleteTask(index) {
+  let tasks = getTasksFromLocalStorage();
+  let confirmDelete = confirm("Are you sure you want to delete this task?");
+
+  if (confirmDelete) {
+    tasks.splice(index, 1);
+
+    saveTasksToLocalStorage(tasks);
+
+    displayTasks();
+  }
+}
+
+function toggleTask(index) {
+  let tasks = getTasksFromLocalStorage();
+
+  tasks[index].completed = !tasks[index].completed;
+
+  saveTasksToLocalStorage(tasks);
+
+  displayTasks();
+}
+
+function getTasksFromLocalStorage() {
+  let tasksJson = localStorage.getItem("tasks");
+  return tasksJson ? JSON.parse(tasksJson) : [];
+}
+
+function saveTasksToLocalStorage(tasks) {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function displayTasks() {
+  let tasks = getTasksFromLocalStorage();
+  let tasksList = document.getElementById("tasks");
+
+  tasksList.innerHTML = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+    let li = document.createElement("li");
+    li.className =
+      "list-group-item d-flex justify-content-between align-items-center";
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = tasks[i].completed;
+    checkbox.addEventListener(
+      "change",
+      (function (index) {
+        return function () {
+          toggleTask(index);
+        };
+      })(i)
+    );
+
+    let taskText = document.createElement("span");
+    taskText.textContent = tasks[i].text;
+    taskText.style.textDecoration = tasks[i].completed
+      ? "line-through"
+      : "none";
+
+    let editButton = document.createElement("button");
+    editButton.className = "btn btn-warning btn-sm";
+    editButton.textContent = "Edit";
+    editButton.onclick = (function (index) {
+      return function () {
+        editTask(index);
+      };
+    })(i);
+
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "btn btn-danger btn-sm";
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = (function (index) {
+      return function () {
+        deleteTask(index);
+      };
+    })(i);
+
+    li.appendChild(checkbox);
+    li.appendChild(taskText);
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+
+    tasksList.appendChild(li);
+  }
+}
+
+function loadTasks() {
+  displayTasks();
 }
