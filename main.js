13dcebd16f0de9c30f11079ded47fcce68fd58cf
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2024-01-16 19:28:39 +0200

    compleated HW 16

--- main.js
+++ main.js
@@ -1,49 +1,79 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const cityNameElement = document.getElementById("city-name");
-  const temperatureElement = document.getElementById("temperature");
-  const pressureElement = document.getElementById("pressure");
-  const descriptionElement = document.getElementById("description");
-  const humidityElement = document.getElementById("humidity");
-  const windSpeedElement = document.getElementById("wind-speed");
-  const windDirectionElement = document.getElementById("wind-direction");
-  const weatherIconElement = document.getElementById("weather-icon");
-  const cityInput = document.getElementById("city-input");
-  const getWeatherBtn = document.getElementById("get-weather-btn");
+  const postIdInput = document.getElementById("postIdInput");
+  const searchPostBtn = document.getElementById("searchPostBtn");
+  const postContainer = document.getElementById("post-container");
+  const postInfo = document.getElementById("post-info");
+  const getCommentsBtn = document.getElementById("getCommentsBtn");
+  const commentsContainer = document.getElementById("comments-container");
 
-  getWeatherBtn.addEventListener("click", function () {
-    const cityName = cityInput.value.trim();
-    if (cityName !== "") {
-      const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=5d066958a60d315387d9492393935c19`;
-      getWeatherData(apiUrl);
+  searchPostBtn.addEventListener("click", function () {
+    const postId = postIdInput.value;
+
+    if (postId >= 1 && postId <= 100) {
+      getPost(postId).then(displayPost).catch(handleError);
     } else {
-      alert("Будь ласка, введіть назву міста.");
+      alert("Введіть ID від 1 до 100");
     }
   });
 
-  function getWeatherData(url) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          const data = JSON.parse(xhr.responseText);
-          displayWeatherData(data);
-        } else {
-          alert("Помилка отримання даних про погоду.");
-        }
-      }
-    };
-    xhr.send();
+  getCommentsBtn.addEventListener("click", function () {
+    const postId = postIdInput.value;
+
+    getComments(postId).then(displayComments).catch(handleError);
+  });
+
+  function getPost(postId) {
+    return new Promise((resolve, reject) => {
+      const apiUrl = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((error) => reject(error));
+    });
   }
 
-  function displayWeatherData(data) {
-    cityNameElement.textContent = data.name;
-    temperatureElement.textContent = data.main.temp;
-    pressureElement.textContent = data.main.pressure;
-    descriptionElement.textContent = data.weather[0].description;
-    humidityElement.textContent = data.main.humidity;
-    windSpeedElement.textContent = data.wind.speed;
-    windDirectionElement.textContent = data.wind.deg;
-    weatherIconElement.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  function getComments(postId) {
+    return new Promise((resolve, reject) => {
+      const apiUrl = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`;
+
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((error) => reject(error));
+    });
+  }
+
+  function displayPost(post) {
+    postInfo.innerHTML = `
+      <p><strong>Title:</strong> ${post.title}</p>
+      <p><strong>Body:</strong> ${post.body}</p>
+    `;
+
+    postContainer.style.display = "block";
+  }
+
+  function displayComments(comments) {
+    commentsContainer.innerHTML = "<h3>Коментарі</h3>";
+
+    if (comments.length > 0) {
+      comments.forEach((comment) => {
+        const commentElement = document.createElement("div");
+        commentElement.classList.add("comment");
+        commentElement.innerHTML = `
+          <p><strong>Name:</strong> ${comment.name}</p>
+          <p><strong>Email:</strong> ${comment.email}</p>
+          <p><strong>Body:</strong> ${comment.body}</p>
+        `;
+        commentsContainer.appendChild(commentElement);
+      });
+    } else {
+      commentsContainer.innerHTML = "<p>Коментарі відсутні.</p>";
+    }
+  }
+
+  function handleError(error) {
+    console.error("Помилка:", error);
+    alert("Помилка при отриманні даних. Будь ласка, спробуйте ще раз.");
   }
 });
