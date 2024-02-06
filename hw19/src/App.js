f3d1870e8ad5f44d5b4a240fcc239fce6889fd51
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2024-02-06 20:49:57 +0200

    compleated HW19

--- hw19/src/App.js
+++ hw19/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Content from "./components/Content/Content";
 
 function App() {
+  const menuItem = ["Home", "Product", "About", "Contact"];
+  const [currentItem, setCurrentItem] = useState(menuItem[0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div>Logo</div>
       </header>
+      <main className="main">
+        <div className="nav">
+          <ul>
+            {menuItem.map((el, i) => (
+              <li key={i} onClick={() => setCurrentItem(el)}>
+                {el}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="content">
+          <Content currentItem={currentItem} />
+        </div>
+      </main>
+      <footer className="footer">Home work 19. Nikita Puzyrenko</footer>
     </div>
   );
 }
