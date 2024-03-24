e2827d7b46415ca417ad336bdf8dd2a01010b095
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2024-03-24 16:41:58 +0200

    refactoring styles

--- src/componets/Share/Share.tsx
+++ src/componets/Share/Share.tsx
@@ -1,11 +1,11 @@
-import style from "./Share.module.scss";
+import style from './Share.module.scss'
 
 type Props = {
-  handleShare: (bool: boolean) => void;
-  id: number;
-  image: string;
-  title: string;
-};
+  handleShare: (bool: boolean) => void
+  id: number
+  image: string
+  title: string
+}
 const Share = ({ handleShare, id, image, title }: Props) => {
   return (
     <>
@@ -15,19 +15,15 @@ const Share = ({ handleShare, id, image, title }: Props) => {
         </div>
         <div className={style.info}>
           <div className={style.img}>
-            <img src={image} alt="Art" />
+            <img src={image} alt='Art' />
           </div>
           <div className={style.title}>{title}</div>
         </div>
         <div className={style.thisLink}>
-          <input
-            type="text"
-            value={`http://localhost:3000/articles/${id}`}
-            disabled
-          />
+          <input type='text' value={`http://localhost:3000/articles/${id}`} />
         </div>
       </div>
     </>
-  );
-};
-export default Share;
+  )
+}
+export default Share
