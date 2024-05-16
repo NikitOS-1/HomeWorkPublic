fd0553aa81cebf7204aac20cee8b15b4dd4be585
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2024-05-16 22:26:36 +0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './container/App/
+import App from './container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
